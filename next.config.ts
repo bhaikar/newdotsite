226d5556031e1e734c78d1a6b93bff75b969ef3b
Dheeraj <dkbhaikar@gmail.com>
2025-09-21 18:47:24 +0530

    Clean up config and package.json

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",   // enables static export
-  images: {
-    unoptimized: true,
+  // The name of your GitHub repo
+  basePath: '/newdotsite',
+  assetPrefix: '/newdotsite/',
+  experimental: {
+    appDir: true,
   },
-  basePath: "/newdotsite",  // IMPORTANT: replace with your repo name
-  assetPrefix: "/newdotsite",
 };
 
 module.exports = nextConfig;
